2dab13a65fa237c444a3337229fabfb9523e8aa6
lingshin <yokaringx@outlook.com>
2025-11-15 22:04:24 +0800

    format(utils)

--- utils/flakegen.nix
+++ utils/flakegen.nix
@@ -1,6 +1,7 @@
 with builtins;
-file: toFile "flake.nix" ''
-  {
-    inputs = ${(import ./pretty.nix).show' "  " (import file).inputs};
-    outputs = inputs: (import ./flake.in.nix).outputs inputs;
-  }''
+  file:
+    toFile "flake.nix" ''
+      {
+        inputs = ${(import ./pretty.nix).show' "  " (import file).inputs};
+        outputs = inputs: (import ./flake.in.nix).outputs inputs;
+      }''
--- utils/pretty.nix
+++ utils/pretty.nix
@@ -6,8 +6,7 @@ with builtins; rec {
     then "true"
     else "false";
 
-  showList = prefix: xs:
-  let
+  showList = prefix: xs: let
     xs' = map (show' prefix) xs;
     intended = intend prefix;
   in
@@ -16,24 +15,21 @@ with builtins; rec {
       [
       ${concatStringsSep "\n" (map (x: intended + show' intended x) xs)}
       ${prefix}]''
-    else"[ ${toString xs'} ]";
+    else "[ ${toString xs'} ]";
 
   mapAttrsToList = f: x: attrValues (mapAttrs f x);
 
-
-
-  showAttrs = prefix: x:
-  let
+  showAttrs = prefix: x: let
     intended = intend prefix;
     showAttr = n: v: intended + n + showChild intended v + ";";
-    showChild = prefix: x:
-      let
-        names = attrNames x;
-      in
+    showChild = prefix: x: let
+      names = attrNames x;
+    in
       if isAttrs x && length names == 1
-      then
-        let name = head names;
-        in "." + name + showChild prefix x.${name}
+      then let
+        name = head names;
+      in
+        "." + name + showChild prefix x.${name}
       else " = " + show' prefix x;
   in ''
     {
