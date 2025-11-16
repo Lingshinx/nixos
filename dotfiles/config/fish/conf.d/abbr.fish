2b751a0a864a9ba5d458b9debc38844bfac957f9
lingshin <yokaringx@outlook.com>
2025-11-16 14:38:20 +0800

    feat(fish): use eza instead

--- dotfiles/config/fish/conf.d/abbr.fish
+++ dotfiles/config/fish/conf.d/abbr.fish
@@ -3,9 +3,9 @@ abbr -a .... -- ../../..
 
 abbr -a plz -- sudo
 
-abbr -a ls -- lsd
-abbr -a la -- lsd -a
-abbr -a ll -- lsd -l
-abbr -a tree -- lsd --tree --depth=2
+abbr -a ls -- eza
+abbr -a la -- eza -a
+abbr -a ll -- eza -l
+abbr -a tree -- eza --tree --depth=2
 
 abbr -a rm -- gomi
