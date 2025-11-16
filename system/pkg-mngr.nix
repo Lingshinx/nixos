99699e585715c6c9151d55d589f49549775ef5a3
lingshin <yokaringx@outlook.com>
2025-11-16 11:31:19 +0800

    add(nix.substituter): add sjtu

--- system/pkg-mngr.nix
+++ system/pkg-mngr.nix
@@ -13,6 +13,7 @@
         "flakes"
       ];
       substituters = [
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
         "https://cache.nixos.org/"
