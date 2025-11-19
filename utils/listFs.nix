472aa9ea9c47e7a01938b426c3df4805bb61a233
lingshin <yokaringx@outlook.com>
2025-11-20 03:02:41 +0800

    feat(utils.listFs): no need for nixpkgs as parameter

--- utils/listFs.nix
+++ utils/listFs.nix
@@ -1,6 +1,7 @@
-{nixpkgs, ...}: let
-  inherit (builtins) readDir toPath map filter toString attrNames listToAttrs;
-  inherit (nixpkgs.lib) filterAttrs;
+with builtins;
+let
+  filterAttrs = f: xs: listToAttrs (filter (attr: f attr.name attr.value) (map (name: {inherit name; value = xs.${name};}) (attrNames xs)));
+
 in rec {
   listAll = filetype: dir: map (name: "${toString dir}/${name}") (attrNames (filterAttrs (name: type: type == filetype) (readDir dir)));
   listAllFiles = listAll "regular";
