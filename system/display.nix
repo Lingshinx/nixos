ef58cf59d284a2a83e95385aca5905f3573722dd
lingshin <yokaringx@outlook.com>
2025-11-20 02:59:15 +0800

    feat(system.display.sddm): add silentSDDM theme

--- system/display.nix
+++ system/display.nix
@@ -1,9 +1,17 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: let
+  silent-sddm = inputs.silentSDDM.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
+    theme = ../dotfiles/sddm-silent-lingshin.conf;
+    extraBackgrounds = [../assets/dust.jpg];
+  };
+in {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = silent-sddm.pname;
+    extraPackages = silent-sddm.propagatedBuildInputs;
   };
 
   programs.niri.enable = true;
-  environment.systemPackages = [pkgs.xwayland-satellite];
+  environment.systemPackages = [pkgs.xwayland-satellite silent-sddm];
+  qt.enable = true;
 }
