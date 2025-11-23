b27d555a5a557a716e85d3f035e2071ee13d4a0a
lingshin <yokaringx@outlook.com>
2025-11-23 22:03:39 +0800

    fix(dotfiles.scripts.waybar)

--- dotfiles/config/lingshin/scripts/waybar/launch.sh
+++ dotfiles/config/lingshin/scripts/waybar/launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set dir ~/.cache/lingshin/waybar
+set dir (md ~/.cache/lingshin/waybar)
 
 set cur_monitor (niri msg --json focused-output | jq -r .name)
 set way_monitor (jq -r .output < $dir/monitor.jsonc)
