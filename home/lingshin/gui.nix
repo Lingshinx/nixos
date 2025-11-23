aff55c578390b21e168c655d29f8ef026874e7de
lingshin <yokaringx@outlook.com>
2025-11-23 18:56:28 +0800

    feat(lingshin.gui): add theme

--- home/lingshin/gui.nix
+++ home/lingshin/gui.nix
@@ -20,4 +20,27 @@
 
     neovide
   ];
+
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Tokyonight-Light";
+      package = pkgs.tokyonight-gtk-theme;
+    };
+
+    cursorTheme = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+    };
+
+    iconTheme = {
+      name = "WhiteSur";
+      package = pkgs.whitesur-icon-theme;
+    };
+  };
 }
