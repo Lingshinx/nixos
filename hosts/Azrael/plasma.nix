1c2e28a60abaca31b8b4661fae3f6a91efb8daa3
lingshin <yokaringx@outlook.com>
2025-11-25 21:45:17 +0800

    feat(Azrael.plasma): add custom session.desktop

--- hosts/Azrael/plasma.nix
+++ hosts/Azrael/plasma.nix
@@ -1,17 +1,42 @@
-{pkgs, ...}: {
-  services.desktopManager.plasma6.enable = true;
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    konsole
-    ark
-    (lib.getBin qttools)
-    elisa
-    gwenview
-    okular
-    kate
-    baloo-widgets
-    ffmpegthumbs
-    krdp
-    xwaylandvideobridge
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  myPlasma =
+    pkgs.writeTextFile {
+      name = "my-plasma-session.desktop";
+      destination = "/share/wayland-sessions/plasma-lingshin.desktop";
+      text = ''
+        [Desktop Entry]
+        Name=Plasma (Lingshin)
+        Exec=dbus-run-session startplasma-wayland
+        DesktopNames=KDE
+      '';
+    }
+    // {
+      providedSessions = ["plasma-lingshin"];
+    };
+in {
+  services.xserver.displayManager.sessionPackages = [
+    myPlasma
   ];
+
+  services.desktopManager.plasma6 = {
+    enable = true;
+    enableQt5Integration = false;
+  };
+
+  environment.plasma6 = {
+    excludePackages = with pkgs.kdePackages; [
+      plasma-browser-integration
+      konsole
+      ark
+      (lib.getBin qttools)
+      elisa
+      okular
+      baloo-widgets
+      krdp
+    ];
+  };
 }
