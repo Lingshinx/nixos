80bf7cd828e93b035883acad0ba893f398fcb463
lingshin <yokaringx@outlook.com>
2025-11-26 23:29:37 +0800

    refactor(lingshin): move git to cli/

--- home/lingshin/cli/default.nix
+++ home/lingshin/cli/default.nix
@@ -26,7 +26,17 @@
     starship
 
     socat
-
-    home-manager
   ];
+
+  programs = {
+    git = {
+      enable = true;
+      settings.user = {
+        name = "lingshin";
+        email = "yokaringx@outlook.com";
+      };
+    };
+
+    home-manager.enable = true;
+  };
 }
--- home/lingshin/config.nix
+++ home/lingshin/config.nix
@@ -13,19 +13,5 @@ in {
     homeDirectory = "/home/lingshin";
   };
 
-  dconf.settings = {};
-
-  programs = {
-    git = {
-      enable = true;
-      settings.user = {
-        name = "lingshin";
-        email = "yokaringx@outlook.com";
-      };
-    };
-
-    home-manager.enable = true;
-  };
-
   home.stateVersion = "25.05";
 }
